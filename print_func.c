18e8a33fe14a2db0fe9151274753f32fa720c446
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-22 21:36:23 -0400

    spell check in some functions

--- print_func.c
+++ print_func.c
@@ -4,30 +4,117 @@
 #include <unistd.h>
 #include "main.h"
 /**
+* printChar - Writes one char to the standard output.
+* @c: just a character
+* Return: the value of write.
+*/
+int printChar(int c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+* printStr - Writes a string to the standard output.
+* @str: just a string
+* Return: the count.
+*/
+int printStr(char *str)
+{
+	int count = 0, i = 0;
+
+	if (str)
+	{
+		while (str[i] != '\0')
+		{
+			printChar(str[i]);
+			count++;
+			i++;
+		}
+	}
+	printChar('\0');
+	return (count);
+}
+
+/**
+* printDigit - Recursively writes a digit to the standard output.
+* @n: number to write
+* @base: 10
+* Return: the count.
+*/
+int printDigit(long n, int base)
+{
+	int count = 0;
+	char *digits = "0123456789";
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		return (printDigit(-n, base));
+	}
+	else if (n < base)
+	{
+		return (printChar(digits[n]));
+	}
+	else
+	{
+		count = printDigit(n / base, base);
+		return (count + printDigit(n % base, base));
+	}
+}
+
+/**
+* specChecker - Checks which specifier is used and redirects to a function.
+* @spec: the character to compare.
+* @ap: the argument pointer
+* Return: the count.
+*/
+int specChecker(char spec, va_list ap)
+{
+	int count = 0;
+
+	if (spec == 'c')
+	{
+		count = printChar(va_arg(ap, int));
+	}
+	else if (spec == 's')
+	{
+		count = printStr(va_arg(ap, char *));
+	}
+	else if (spec == 'i' || spec == 'd')
+	{
+		count = printDigit((long)va_arg(ap, int), 10);
+	}
+	else
+	{
+		count = write(1, &spec, 1);
+	}
+	return (count);
+}
+
+/**
 * _printf - Prints what you need it to.
-* format: the string
+* @format: the string
 * @..: the arguments
 * Return: the count.
 */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int count = 0;
+	int count = 0;
+	va_list ap;
 
-    va_start(ap, format);
-    count = 0;
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            count += specChecker(*(++format), ap);
-        }
-        else
-        {
-            count += write(1, format, 1);
-        }
-        format++;
-    }
-    va_end(ap);
-    return (count);
-    }
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			count += specChecker(*(++format), ap);
+		}
+		else
+		{
+			count += write(1, format, 1);
+		}
+		format++;
+	}
+	va_end(ap);
+	return (count);
+}
